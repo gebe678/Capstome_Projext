7d7e7c1168330011d58f39d90056b255947440a2
Griffin <lehrerg6@gmail.com>
2021-04-07 21:29:05 -0400

    added two block VGG script

--- CNN_Baseline.py
+++ CNN_Baseline.py
@@ -49,9 +49,9 @@ def run_test_harness():
 	# create data generator
 	datagen = ImageDataGenerator(rescale=1.0/255.0)
 	# prepare iterators
-	train_it = datagen.flow_from_directory('/Users/mariamorales/Documents/Capstome_Projext/dogs-vs-catstrain/',
+	train_it = datagen.flow_from_directory('C:/Users/warri/Desktop/Capstone/Capstome_Projext/dogs-vs-catstrain/',
 		class_mode='binary', batch_size=64, target_size=(200, 200))
-	test_it = datagen.flow_from_directory('/Users/mariamorales/Documents/Capstome_Projext/dogs-vs-catstest/',
+	test_it = datagen.flow_from_directory('C:/Users/warri/Desktop/Capstone/Capstome_Projext/dogs-vs-catstest/',
 		class_mode='binary', batch_size=64, target_size=(200, 200))
 	# fit model
 	history = model.fit(train_it, steps_per_epoch=len(train_it),
